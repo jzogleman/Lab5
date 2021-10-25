49b6e96d7b43cb81409ad80f5eb14ed3f5d1c4e3
Josh Zogleman <jzogleman@wisc.edu>
2021-10-25 13:24:50 -0500

    commit

--- app/src/main/java/com/example/lab5milestone1/MainActivity3.java
+++ app/src/main/java/com/example/lab5milestone1/MainActivity3.java
@@ -30,7 +30,8 @@ public class MainActivity3 extends AppCompatActivity {
 
         textView3 = (EditText) findViewById(R.id.textView3);
         Intent intent = getIntent();
-        noteid = Integer.parseInt(intent.getStringExtra("noteid"));
+        noteid = intent.getIntExtra("noteid", -1);
+        Log.i("OnClick", "noteid = " + noteid);
 
 
         if(noteid != -1){
@@ -60,9 +61,11 @@ public class MainActivity3 extends AppCompatActivity {
 
         if(noteid == -1){
             title = "NOTE_" + (MainActivity2.notes.size() + 1);
+            Log.i("OnClick", "New Note");
             helper.saveNotes(username, title, noteContent, date);
         }else {
             title = "NOTE_" + (noteid + 1);
+            Log.i("OnClick", "Updating");
             helper.updateNote(title, date, noteContent, username);
         }
 
